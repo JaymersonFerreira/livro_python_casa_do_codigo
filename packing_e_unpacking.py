20e73ceee868ca35e7c7abeb3746d1db9607ee59
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-05-14 08:41:18 -0300

    Adicionado exemplo com paramentro nomeados

--- packing_e_unpacking.py
+++ packing_e_unpacking.py
@@ -4,7 +4,18 @@ from datetime import date
 d = (2013, 3, 15)
 date(*d)
 
+#parâmetro nomeados
+
+def new_user(active = False, admin = False):
+    print(active)
+    print(admin)
+
+config = {"active": False, "admin":True}
+
+new_user(**config)
+
 '''O unpacking é o precosso que é executado dentro da função, e não na chamada. Podemos usar a sintaxe *args ou **kwargs como argumentos para o unpacking dos parâmentros posicionados ou nomeados'''
+
 
 def unpacking_experiment(*args):
     arg1 = args[0]
@@ -15,4 +26,13 @@ def unpacking_experiment(*args):
     print(others)
 
 unpacking_experiment(1,2,3,4,5,6)
+
+
+#parâmetro nomeados
+
+def unpacking_experiment(**kwargs):
+    print(kwargs)
+
+unpacking_experiment(named="Test", other="Other",)
+{'other': 'Other', 'named': 'Test'}
 
